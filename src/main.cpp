1f7c1abb864e4e315c384aa314fee48bc73317fc
Bahadir Arslan <arslan.bah@gmail.com>
2025-04-06 20:34:07 +0200

    Step 2: Readjusted rotation.

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,14 @@ Vector2 VecSin(Vector2 vec) {
     return (Vector2){sin(DEG2RAD * (45 + abs((int)vec.x) % 90)), sin(DEG2RAD * (45 + abs((int)vec.y % 90)))};
 }
 
+void PauseGame(int milliseconds)
+{
+    int startTime = GetTime();
+    while (GetTime() - startTime < milliseconds) {
+        // Do nothing, just wait
+    }
+}
+
 int main(void)
 {
     // Initialization
@@ -17,10 +25,11 @@ int main(void)
     std::filesystem::path fs = std::filesystem::path(__FILE__).parent_path();
     std::string texturePathStr = (fs / "textures" / "concrete.png").string();
     std::string shaderPathStr = (fs / "vendor/raylib/examples/shaders/resources/shaders/glsl330" / "blur.fs").string();
+    std::string modelPathStr = (fs / "models" / "mr_angry_cube.obj").string();
 
     const int screenWidth = 800;
     const int screenHeight = 450;
-    const float cubeSize = 4.0f;
+    const float cubeSize = 1.0f;
     const float cubeHalfSize = cubeSize / 2.0f;
     const float cubeHypotenuse = sqrt(cubeHalfSize * cubeHalfSize * 2);
 
@@ -31,19 +40,20 @@ int main(void)
     camera.position = (Vector3){ 0.0f, 10.0f, -5.0f };  // Camera position
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
-    // camera.projection = CAMERA_ORTHOGRAPHIC;             // Camera mode type FIXME REMOVE
+    // camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.fovy = 5.0f;                                // Camera field-of-view Y
+    // camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
+    camera.projection = CAMERA_ORTHOGRAPHIC;             // Camera mode type FIXME REMOVE
 
     int quarterRotation = 90;
     int increment = 1;
 
     Vector3 cubePosition = { 0.0f, cubeSize/2, 0.0f };
-    Vector3 rotateAxis = { 0.0f, 0.0f, 1.0f };
+    Vector3 rotateAxis = { 0.0f, 0.0f, 0.0f };
     Vector3 cubeRotation = { 0.0f, 0.0f, 0.0f };
     Vector3 nextRotateAxis = rotateAxis;
 
-    Model model = LoadModel("/media/lyonbach/depot/Work/Projects/codeoflyonbach/MrAngryCubeTest/models/mr_angry_cube.obj");
+    Model model = LoadModel(modelPathStr.c_str());
     Texture texture = LoadTexture(texturePathStr.c_str());
     Shader shader = LoadShader(0, shaderPathStr.c_str());
     Material material = LoadMaterialDefault();
@@ -57,7 +67,7 @@ int main(void)
     //--------------------------------------------------------------------------------------
 
     // Define Variables
-    Matrix transform = MatrixIdentity();
+    Matrix transform = MatrixScale(cubeSize, cubeSize, cubeSize);
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -99,30 +109,30 @@ int main(void)
         // we can use 2d vector to calculate the vertical position of the cube.
         Vector2 deltaY = Vector2Scale(Vector2Scale(VecSin((Vector2){cubeRotation.x, cubeRotation.z}), cubeHypotenuse), 1/cubeHalfSize);
 
+        Vector3 incrementVector = Vector3Scale(rotateAxis, DEG2RAD * increment);
+        transform = MatrixMultiply(transform, MatrixRotateXYZ((Vector3){incrementVector.x, incrementVector.y, incrementVector.z}));
         
-        // transform = MatrixMultiply(MatrixTranslate(0.0f, 0.0f, 0.0f), transform);
-        transform.m12 = -cubeRotation.z / 180.0f * cubeSize;
-        transform.m13 = deltaY.y * deltaY.x;
-        transform.m14 = cubeRotation.x / 180.0f * cubeSize;
-
         // We need to update the cube position via offsetting. So we need to update the transform matrix.
-        transform = MatrixMultiply(transform, MatrixRotate(rotateAxis, DEG2RAD * increment));
+        transform.m12 = -cubeRotation.z / 90.0f * cubeSize * 2;
+        transform.m13 = deltaY.y * deltaY.x * cubeSize;
+        transform.m14 = cubeRotation.x / 90.0f * cubeSize * 2;
 
-        TraceLog(LOG_INFO, "rotation: %f %f %f", cubeRotation.x, cubeRotation.y, cubeRotation.z);
-        TraceLog(LOG_INFO, "height: %f %f", deltaY.x, deltaY.y);
+
+        // TraceLog(LOG_INFO, "rotation: %f %f %f", cubeRotation.x, cubeRotation.y, cubeRotation.z);
+        // TraceLog(LOG_INFO, "height: %f %f", deltaY.x, deltaY.y);
 
 
         // Set next rotation axis when cube is exactly at 90° increments.
         if ((int)cubeRotation.x % quarterRotation == 0 && rotateAxis.x != 0.0f || 
             (int)cubeRotation.z % quarterRotation == 0 && rotateAxis.z != 0.0f ||
-            (int)cubeRotation.y % quarterRotation == 0 && rotateAxis.y != 0.0f)
+            (int)cubeRotation.y % quarterRotation == 0 && rotateAxis.y != 0.0f ||
+            (rotateAxis.x == 0.0f && rotateAxis.z == 0.0f && rotateAxis.y == 0.0f))
         {
             rotateAxis = nextRotateAxis;
         }
 
-
         camera.target = (Vector3){transform.m12, transform.m13, transform.m14};
-        camera.position = (Vector3){camera.target.x, camera.target.y + 5, camera.target.z - 10.0f};
+        camera.position = (Vector3){camera.target.x, camera.target.y , camera.target.z - 10.0f};
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -144,7 +154,7 @@ int main(void)
     // De-Initialization
     //--------------------------------------------------------------------------------------
     UnloadShader(shader);       // Unload shader
-    // UnloadTexture(texture);     // Unload texture
+    UnloadTexture(texture);     // Unload texture
     UnloadModel(model);         // Unload model
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
